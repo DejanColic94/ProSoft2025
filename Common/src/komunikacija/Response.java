5f4f0039b9af873be07fd97cd55f6bf8976d1700
DEJAN\38163 <cola_dcx@yahoo.com>
2025-07-31 23:29:34 +0200

    implemented constants and request/response

---
+++ Common/src/komunikacija/Response.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package komunikacija;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Dejan Colic
+ */
+public class Response implements Serializable{
+    private Object params;
+    private boolean success;
+    private String message;
+
+    public Response() {
+    }
+
+    public Response(Object params, boolean success, String message) {
+        this.params = params;
+        this.success = success;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getParams() {
+        return params;
+    }
+
+    public void setParams(Object params) {
+        this.params = params;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+    
+    
+    
+}
